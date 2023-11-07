fd422ab5a7adaa3558786d65de6f8ab6d126703d
EC2 Default User <ec2-user@ip-172-31-15-97.ap-northeast-1.compute.internal>
2023-11-07 16:02:14 +0000

    [finish]-18,19

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_02_113514) do
+ActiveRecord::Schema.define(version: 2023_11_07_152118) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,22 +40,22 @@ ActiveRecord::Schema.define(version: 2023_11_02_113514) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "m_eshiterros", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "post_images", force: :cascade do |t|
-    t.string "shop_name"
-    t.text "caption"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "postimages", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
     t.integer "user_id"
