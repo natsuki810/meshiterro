ac00938270dd9d536e7fb2bca1e508921bad383c
EC2 Default User <ec2-user@ip-172-31-15-97.ap-northeast-1.compute.internal>
2023-11-12 13:04:57 +0000

    [fibish]-meshiterro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  #before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
   
   def update
     @user = User.find(params[:id])
@@ -19,4 +21,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id ==current_user.id
+      redirect_to post_images_path
+    end
+  end
+ end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ import "channels"
 
 import "jquery";
 import "popper.js";
-import "boosttrap";
+import "bootstrap";
 import "../stylesheets/application";
 
 Rails.start()
